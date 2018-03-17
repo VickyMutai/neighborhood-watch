f1537ce2aa7f33b92a43e58a1651346f422e63b8
VickyMutai <vicky.mutai96@gmail.com>
2018-03-17 18:04:29 +0300

    setting up django-registration

--- neighbor/settings.py
+++ neighbor/settings.py
@@ -79,12 +79,17 @@ WSGI_APPLICATION = 'neighbor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'watch',
+        'USER': 'vicky',
+        'PASSWORD':'0716889165',
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+#email settings
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
