0de6af39a73087fc4e03e43a628bbbf412455465
VickyMutai <vicky.mutai96@gmail.com>
2018-03-19 14:00:08 +0300

    addinng setup,teardown and testinstance tests for neighborhood model

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Neighborhood
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class NeighborhoodTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        #set up user class
+        self.new_user = User(username='vic',email='vic@mail.com')
+        self.new_user.save()
+        #set up neighborhood class
+        self.embakasi = Neighborhood(name='pipeline',location='embakasi',occupants_count=12,admin=self.new_user)
+        self.embakasi.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Neighborhood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.embakasi,Neighborhood))
