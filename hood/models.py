03cc4ec8c078429328887f6140a656916f12fd85
VickyMutai <vicky.mutai96@gmail.com>
2018-03-21 03:08:12 +0300

    migrate new changes to db

--- hood/models.py
+++ hood/models.py
@@ -7,6 +7,8 @@ class Neighborhood(models.Model):
     name =  models.CharField(max_length=60)
     location = models.CharField(max_length=60)
     occupants_count = models.PositiveIntegerField(default=0)
+    health_number = models.CharField(max_length=20)
+    police_number = models.CharField(max_length=20)
     admin = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
